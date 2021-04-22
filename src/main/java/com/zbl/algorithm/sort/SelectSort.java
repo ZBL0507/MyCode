34ec51638e2ffeec2ef2d4521e3f38857117b0a7
zbl <987693976@qq.com>
2021-04-22 16:45:32 +0800

    '选择排序完成'

---
+++ src/main/java/com/zbl/algorithm/sort/SelectSort.java
@@ -0,0 +1,26 @@
+package com.zbl.algorithm.sort;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = {234, 456547, 879, 890, 0, 4, 76, -3, -4375, 657, 234324, 457};
+        selectSort(arr);
+    }
+
+    /**
+     * 选择排序
+     *
+     * @param arr 待排序的数组，这里使用整形演示算法
+     */
+    public static void selectSort(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
