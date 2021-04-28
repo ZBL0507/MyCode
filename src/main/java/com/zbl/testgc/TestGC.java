3fd2d59171b38d903b57a95f98ed8678e7bdadf8
zbl <987693976@qq.com>
2021-04-28 15:02:19 +0800

    GC测试小程序

--- src/main/java/com/zbl/testgc/TestGC.java
+++ src/main/java/com/zbl/testgc/TestGC.java
@@ -1,4 +1,16 @@
 package com.zbl.testgc;
 
+/**
+ * -XX:InitialHeapSize=16777216
+ * -XX:MaxHeapSize=16777216
+ * -XX:+PrintCommandLineFlags
+ * -XX:+PrintGCDetails
+ * -XX:-UseLargePagesIndividualAllocation
+ */
+@SuppressWarnings("unused")
 public class TestGC {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+        byte[] bytes = new byte[10944 * 1024];
+    }
 }
