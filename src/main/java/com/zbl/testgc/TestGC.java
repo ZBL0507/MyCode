a18dd048c8b8fd3afb19a096b5e5a03aa252a657
zbl <987693976@qq.com>
2021-04-29 15:21:31 +0800

    更新txt，增加垃圾回收器概念

--- src/main/java/com/zbl/testgc/TestGC.java
+++ src/main/java/com/zbl/testgc/TestGC.java
@@ -9,8 +9,9 @@ package com.zbl.testgc;
  */
 @SuppressWarnings("unused")
 public class TestGC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("hhh");
-        byte[] bytes = new byte[10944 * 1024];
+        byte[] bytes = new byte[5 * 1024 * 1024];
+        Thread.sleep(Integer.MAX_VALUE);
     }
 }
