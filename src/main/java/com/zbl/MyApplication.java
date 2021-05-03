e40f6086fc3c17d29c1da18b6fefb5a7198a1ba2
zbl <987693976@zbl.com>
2021-05-03 11:38:46 +0800

    循环依赖问题演示

---
+++ src/main/java/com/zbl/MyApplication.java
@@ -0,0 +1,17 @@
+package com.zbl;
+
+import com.zbl.circular.dependency.A;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SuppressWarnings("unused")
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(MyApplication.class, args);
+        A a = context.getBean("a", A.class);
+
+        Object b = context.getBean("b");
+    }
+}
