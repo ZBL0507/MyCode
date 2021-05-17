dddd05f8f76e2f2526eb54d5eac64553efc75789
zbl <987693976@qq.com>
2021-05-17 18:30:46 +0800

    更新笔记

---
+++ src/main/java/com/zbl/algorithm/leetcode/Test0022.java
@@ -0,0 +1,59 @@
+package com.zbl.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树的层序遍历
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层序遍历结果：
+ *
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnldjj/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+@SuppressWarnings("unused")
+public class Test0022 {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+
+    static class TreeNode {
+        int val;
+        Test0021.TreeNode left;
+        Test0021.TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, Test0021.TreeNode left, Test0021.TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
