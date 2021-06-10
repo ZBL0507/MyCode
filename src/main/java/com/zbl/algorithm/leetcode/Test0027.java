128292ea49af2f4154fa4d3197dc9966fafd8132
zbl <987693976@qq.com>
2021-06-10 16:51:25 +0800

    LeetCode 二叉树的前序遍历

---
+++ src/main/java/com/zbl/algorithm/leetcode/Test0027.java
@@ -0,0 +1,36 @@
+package com.zbl.algorithm.leetcode;
+
+import com.zbl.ds.structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树的前序遍历
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ */
+public class Test0027 {
+    public static void main(String[] args) {
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode1 = new TreeNode(1);
+        treeNode3.left = treeNode2;
+        treeNode3.right = treeNode1;
+        List<Integer> list = preorderTraversal(treeNode3);
+        System.out.println(list);
+    }
+
+    private static List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        process(root, list);
+        return list;
+    }
+
+    private static void process(TreeNode node, List<Integer> list) {
+        if (null == node)
+            return;
+        list.add(node.val);
+        process(node.left, list);
+        process(node.right, list);
+    }
+}
