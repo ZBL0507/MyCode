3c7253d82d76e183a8ce33b4796464de8eed6096
zbl <987693976@qq.com>
2021-06-10 14:29:03 +0800

    数据结构 动态数组完善

--- src/main/java/com/zbl/ds/container/DynamicArray.java
+++ src/main/java/com/zbl/ds/container/DynamicArray.java
@@ -1,5 +1,6 @@
 package com.zbl.ds.container;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -24,7 +25,7 @@ public class DynamicArray<T> {
     private int size;
 
     /**
-     * array的长度
+     * array的长度，即动态数组的容量
      */
     private int capacity;
 
@@ -133,6 +134,25 @@ public class DynamicArray<T> {
     }
 
     /**
+     * 判断数组是否为空
+     *
+     * @return 如果为空返回true，否则返回false
+     */
+    @SuppressWarnings("all")
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    /**
+     * 判断数组是否不空
+     *
+     * @return 如果为不空返回true，否则返回false
+     */
+    public boolean isNotEmpty() {
+        return !isEmpty();
+    }
+
+    /**
      * 获取指定下标上的元素
      *
      * @param index 下标
@@ -150,10 +170,14 @@ public class DynamicArray<T> {
 
     @Override
     public String toString() {
+        if (size == 0)
+            return "[ ]";
+
         StringBuilder builder = new StringBuilder("[");
         for (int i = 0; i < size; i++) {
-            builder.append(array[i]).append("\t");
+            builder.append(array[i]).append("  ");
         }
+        builder.deleteCharAt(builder.length() - 1);
         builder.deleteCharAt(builder.length() - 1);
         builder.append("]");
         return builder.toString();
@@ -179,6 +203,7 @@ public class DynamicArray<T> {
      * @param values 待加入的多个元素
      * @return 加入成功返回true，否则返回false
      */
+    @SuppressWarnings("all")
     public boolean addAll(T... values) {
         Arrays.stream(values).forEach(this::add);
         return true;
@@ -207,6 +232,21 @@ public class DynamicArray<T> {
         return arr;
     }
 
+    /**
+     * 将动态数组转化为普通数组
+     *
+     * @param newType 要转化的数组类型
+     * @return 普通数组
+     */
+    public T[] toArray(Class<? extends T[]> newType) {
+        @SuppressWarnings("all")
+        T[] arr = ((Object) newType == (Object) Object[].class)
+                ? (T[]) new Object[size]
+                : (T[]) Array.newInstance(newType.getComponentType(), size);
+        System.arraycopy(array, 0, arr, 0, arr.length);
+        return arr;
+    }
+
 
     /**
      * array扩容
@@ -229,5 +269,86 @@ public class DynamicArray<T> {
         if (size >= 0)
             System.arraycopy(oldArray, 0, newArray, 0, size);
         this.array = newArray;  // array去引用新的数组
+    }
+
+    /**
+     * Removes all of the elements from this list.  The list will
+     * be empty after this call returns.
+     */
+    public void clear() {
+        // clear to let GC do its work
+        for (int i = 0; i < size; i++)
+            array[i] = null;
+
+        size = 0;
+    }
+
+    /**
+     * 在动态数组中删除指定的元素（只删除第一次出现的）
+     *
+     * @param value 指定的元素
+     * @return 删除成功返回true，删除失败返回false，找不到指定的元素也认为删除成功
+     */
+    @SuppressWarnings("all")
+    public boolean remove(T value) {
+        if (null == value)
+            throw new NullPointerException();
+
+        for (int i = 0; i < size; i++)
+            if (value.equals(array[i])) {
+                if (size - 1 - i > 0)
+                    System.arraycopy(array, i + 1, array, i, array.length - 1 - i);
+
+                array[--size] = null; // clear to let GC do its work
+                return true;
+            }
+
+        return true;
+    }
+
+    /**
+     * 删除动态数组中指定下标上的元素，并返回这个删除的元素
+     *
+     * @param index 指定的下标
+     * @return 删除的元素
+     */
+    public T remove(int index) {
+        if (index < 0)
+            throw new IllegalArgumentException("" + index);
+
+        if (index >= size)
+            throw new IndexOutOfBoundsException("" + index);
+
+        //保存待删除的元素
+        T value = array[index];
+
+        if (size - 1 - index > 0)
+            System.arraycopy(array, index + 1, array, index, array.length - 1 - index);
+
+        array[--size] = null; // clear to let GC do its work
+
+        return value;
+    }
+
+    /**
+     * 删除动态数组中第一个元素，并返回
+     *
+     * @return 返回动态数组中第一个元素，若动态数组为空则返回null
+     */
+    public T removeFirstElement() {
+        if (0 == size)
+            return null;
+        return remove(0);
+    }
+
+    /**
+     * 删除动态数组中最后一个元素，并返回
+     *
+     * @return 返回动态数组中最后一个元素，若动态数组为空则返回null
+     */
+    public T removeLastElement() {
+        if (0 == size)
+            return null;
+        return remove(size - 1);
     }
 }
--- src/test/java/com/zbl/mytest/container/DynamicArrayTest.java
+++ src/test/java/com/zbl/mytest/container/DynamicArrayTest.java
@@ -1,5 +1,6 @@
 package com.zbl.mytest.container;
 
+import com.zbl.circular.dependency.CC;
 import com.zbl.ds.container.DynamicArray;
 import com.zbl.util.ArrayUtils;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+@SuppressWarnings("unused")
 public class DynamicArrayTest {
     @Test
     public void add() {
@@ -42,8 +44,7 @@ public class DynamicArrayTest {
         String s = array.get(4);
         System.out.println(s);
         System.out.println(array.toString());
-        Object[] objects = array.toArray();
-        ArrayUtils.printArr(objects);
+
     }
 
     @Test
@@ -61,10 +62,12 @@ public class DynamicArrayTest {
     }
 
     @Test
-    public void myTest() {
+    public void add8() {
         Integer[] integers = {12, 2445, 4, 675, 78, 89};
         DynamicArray<Integer> array = new DynamicArray<>(integers);
         System.out.println(array);
+        Integer[] toArray = array.toArray(integers.getClass());
+        ArrayUtils.printArr(toArray);
     }
 
     @Test
@@ -73,5 +76,93 @@ public class DynamicArrayTest {
         DynamicArray<String> array = new DynamicArray<>();
         array.addAll(list);
         System.out.println(array);
+    }
+
+    @Test
+    public void add7() {
+        List<String> list = Arrays.asList("23", "34", "2345", "sdu");
+        DynamicArray<String> array = new DynamicArray<>();
+        System.out.println(array.addAll(list));
+        System.out.println(array);
+        String[] strings = array.toArray(String[].class);
+        ArrayUtils.printArr(strings);
+    }
+
+    @Test
+    public void remove() {
+        DynamicArray<Integer> array = new DynamicArray<>();
+        array.addAll(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2);
+
+        System.out.println("当前数组大小：" + array.size());
+        while (!array.isEmpty()) {
+            array.remove(2);
+            System.out.println("当前数组大小：" + array.size());
+        }
+
+        System.out.println("当前数组大小：" + array.size());
+        array.remove(2);
+        System.out.println("当前数组大小：" + array.size());
+        array.remove(2);
+        System.out.println("当前数组大小：" + array.size());
+        array.remove(2);
+        System.out.println("当前数组大小：" + array.size());
+        array.remove(2);
+        System.out.println("当前数组大小：" + array.size());
+        array.remove(2);
+        System.out.println("当前数组大小：" + array.size());
+        array.remove(2);
+    }
+
+    @Test
+    public void remove2() {
+        DynamicArray<String> array = new DynamicArray<>();
+        array.addAll("haha", "haha", "haha", "haha", "haha", "haha", "haha", "haha");
+        System.out.println("当前数组大小：" + array.size());
+        while (array.isNotEmpty()) {
+            array.remove("haha");
+            System.out.println("当前数组大小：" + array.size());
+        }
+    }
+
+    @Test
+    public void remove3() {
+        DynamicArray<CC> array = new DynamicArray<>();
+        CC c1 = new CC();
+        array.add(c1);
+        CC c2 = new CC();
+        array.add(c2);
+        array.remove(c2);
+        System.out.println(array);
+        array.remove(c1);
+        System.out.println(array);
+    }
+
+    @Test
+    public void remove4() {
+        DynamicArray<Integer> array = new DynamicArray<>();
+        array.addAll(1, 23, 4, 5, 6, 6, 77, 7);
+        System.out.println(array);
+        Integer remove = array.remove(2);
+        System.out.println(remove);
+        System.out.println(array);
+        array.remove(array.size() - 1);
+        System.out.println(array);
+    }
+
+    @Test
+    public void remove5() {
+        DynamicArray<Integer> array = new DynamicArray<>();
+        array.addAll(123, 324, 3465, 4576, 767678, 78989, 345, 456);
+        while (array.isNotEmpty())
+            System.out.println(array.removeLastElement());
+    }
+
+    @Test
+    public void remove6() {
+        DynamicArray<Integer> array = new DynamicArray<>();
+        boolean b = array.addAll(123, 324, 3465, 4576, 767678, 78989, 345, 456);
+        System.out.println(b);
+        while (array.isNotEmpty())
+            System.out.println(array.removeFirstElement());
     }
 }
