9ac297d6d2d3fd2e6717dc1d53f55c0e20ba34b6
zbl <987693976@zbl.com>
2021-09-05 10:34:46 +0800

    LeetCode 两数相加

--- src/main/java/com/zbl/ds/structure/ListNode.java
+++ src/main/java/com/zbl/ds/structure/ListNode.java
@@ -16,4 +16,20 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        ListNode p = this;
+        while (p != null) {
+            builder.append(p.val).append(", ");
+            p = p.next;
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        builder.deleteCharAt(builder.length() - 1);
+        builder.append("]");
+
+        return builder.toString();
+    }
 }
