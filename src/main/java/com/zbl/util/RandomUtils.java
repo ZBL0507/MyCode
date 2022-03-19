bb00a0caf1b9309d6a13caea7527b65d77007c4a
zbl_macOS <1714217989@qq.com>
2022-03-19 13:41:33 +0800

    算法练习,工具补充

--- src/main/java/com/zbl/util/RandomUtils.java
+++ src/main/java/com/zbl/util/RandomUtils.java
@@ -20,4 +20,18 @@ public class RandomUtils {
         return Math.min(low, hig) + random.nextInt(Math.abs(hig - low) + 1);
     }
 
+
+    /**
+     * 随机生成一个指定长度的int型数组
+     *
+     * @param length 指定的长度
+     * @return 随机生成的数组
+     */
+    public static int[] randomIntArray(int length) {
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = randomInt(0, Integer.MAX_VALUE - 1);
+        }
+        return arr;
+    }
 }
